adb7d493b5b7ee06ea483618a2ee736c52f39792
CsGo-Kirill <limon402024@gmail.com>
2026-09-13 21:23:28 +0300

    main_tg.py

--- main_tg.py
+++ main_tg.py
@@ -248,7 +248,3 @@ async def handle_message(message: types.Message):
 async def main():
     print("Бот запущен.")
     await dp.start_polling(bot)
-
-
-if __name__ == "__main__":
-    asyncio.run(main())
